aa5de7847df362fdf56aef23c9042d4666c1fabe
Alexander Sagen <alexander@sagen.me>
2023-10-24 01:32:45 +0200

    Remove unneccessary import

--- scripts/test.php
+++ scripts/test.php
@@ -2,7 +2,6 @@
 declare(strict_types = 1);
 namespace ObieSampleApp;
 use Obie\Log;
-use Obie\View;
 
 /** @var App $app */
 if (!($app = require dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'init.php')) return;
